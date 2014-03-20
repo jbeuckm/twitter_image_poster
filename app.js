283cffcfc5742d35832c7240ac97af55c02975db
joe beuckman <joebeuckman@gmail.com>
2014-03-19 20:17:29 -0500

    fixed indentation on app.js

--- app.js
+++ app.js
@@ -2,36 +2,33 @@ var Twitter = require('node-twitter');
 var config = require('./config.json');
 
 var twitterRestClient = new Twitter.RestClient(
-					       config.consumer_key,
-					       config.consumer_secret,
-					       config.access_token,
-					       config.access_secret
+  config.consumer_key,
+  config.consumer_secret,
+  config.access_token,
+  config.access_secret
 );
 
-function post(status, image){
+function post(status, image) {
 
-twitterRestClient.statusesUpdateWithMedia(
-					   {
-					       'status': status,
-						   'media[]': image
-						   },
-					   function(error, result) {
-					   if (error)
-					       {
-						   console.log('Error: ' + (error.code ? error.code + ' ' + error.message : error.message));
-					       }
+  twitterRestClient.statusesUpdateWithMedia(
+    {
+      'status': status,
+      'media[]': image
+    },
+    function (error, result) {
+      if (error) {
+        console.log('Error: ' + (error.code ? error.code + ' ' + error.message : error.message));
+      }
 
-					   if (result)
-					       {
-						   console.log(result);
-						   console.log(twitterRestClient);
-					       }
-					   });
-
+      if (result) {
+        console.log(result);
+        console.log(twitterRestClient);
+      }
+    });
 
 
 }
 
-setInterval(function(){
-	post("Posting a tweet w/ attached media.", "./image/png");
-    }, 30000);
+setInterval(function () {
+  post("Posting a tweet w/ attached media.", "./image/png");
+}, 30000);
